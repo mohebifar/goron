8f2a5f39cc2e8f53131e238d14f5732d4f5cb322
mohamad <netfars@gmail.com>
2015-09-10 13:44:01 +0430

    feat: Basic structure for electron/react

---
+++ src/config/index.js
@@ -0,0 +1,8 @@
+import path from 'path';
+
+export default {
+  path: {
+    resources: path.dirname(path.dirname(__dirname)) + '/res',
+    views: path.dirname(path.dirname(__dirname)) + '/res/views'
+  }
+}
---
+++ src/utils/electron/window.js
@@ -0,0 +1,41 @@
+import config from './../../config';
+import BrowserWindow from 'browser-window';
+
+export default
+class Window {
+  constructor(name) {
+    this.properties = {
+      width: 640,
+      height: 480,
+      'min-width': 640,
+      'min-height': 480
+    };
+
+    this.window = null;
+    this.name = name;
+    this.setView('main.html');
+  }
+
+  setView(view) {
+    this.url = 'file:///' + config.path.views + '/' + view;
+
+    if (this.window) {
+      this.window.loadUrl(this.url);
+    }
+
+    return this;
+  }
+
+  set(key, value) {
+    this.properties[key] = value;
+  }
+
+  create() {
+    this.window = new BrowserWindow(this.properties);
+    this.window.loadUrl(this.url);
+
+    this.window.webContents.on('did-finish-load', () => {
+      this.window.webContents.send('app', this.name);
+    });
+  }
+}
