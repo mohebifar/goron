f976a13c17b953137ba4fd5bfa7d3f94efb72b21
mohamad <netfars@gmail.com>
2015-09-11 19:15:54 +0430

    style: Make a sample style for connections view

--- src/windows/connections.jsx
+++ src/windows/connections.jsx
@@ -1,17 +1,18 @@
 import React, {Component, PropTypes} from 'react';
-import {AppBar, Dialog} from 'material-ui';
+import {AppBar, List, ListItem, DropDownIcon, IconMenu, IconButton} from 'material-ui';
+import MenuItem from 'material-ui/lib/menus/menu-item.js'
+import RemoveIcon from 'material-ui/lib/svg-icons/action/delete.js'
+import EditIcon from 'material-ui/lib/svg-icons/editor/mode-edit.js'
+import AddIcon from 'material-ui/lib/svg-icons/content/add-box.js'
 
 var mui = require('material-ui');
 var ThemeManager = new mui.Styles.ThemeManager();
-var Colors = mui.Styles.Colors;
-ThemeManager.setPalette({
-  primary1Color: Colors.blueGrey500,
-  accent1Color: Colors.deepOrange500
-});
 
-export default class Connections extends Component {
-
-  static childContextTypes = {muiTheme: PropTypes.object}
+export default
+class Connections extends Component {
+  static childContextTypes = {
+    muiTheme: PropTypes.object
+  };
 
   getChildContext() {
     return {
@@ -20,20 +21,27 @@ export default class Connections extends Component {
   }
 
   componentWillMount() {
-    ThemeManager.setPalette({
-      primary1Color: Colors.blueGrey500,
-      accent1Color: Colors.deepOrange500
-    });
   }
 
   render() {
-    var standardActions = [
-      { text: 'Cancel' },
-      { text: 'Submit', onClick: this._onDialogSubmit, ref: 'submit' }
-    ];
-
     return <div>
-      <AppBar title='Pac' style={{position: 'fixed', top: 0}} />
+      <AppBar title="Goron" iconElementLeft={<div></div>} iconElementRight={<IconButton><AddIcon /></IconButton>} />
+      <List>
+        <ListItem primaryText="New Connection" rightIconButton={
+          <div>
+          <IconButton tooltip="Delete" touch={true} tooltipPosition="bottom-left">
+            <RemoveIcon />
+          </IconButton>
+          <IconButton tooltip="Edit" touch={true} tooltipPosition="bottom-left">
+            <EditIcon />
+          </IconButton>
+            </div>}
+          secondaryText={
+            <p>
+              <span>127.0.0.1:3771</span>
+            </p>
+            }/>
+      </List>
     </div>
   }
 }
