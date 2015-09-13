161d35d2e607935100ee25cf7d276bd967de9990
mohamad <netfars@gmail.com>
2015-09-13 23:48:16 +0430

    fix: Apply react-tap-even-plugin to fix tap problem

--- src/entry.jsx
+++ src/entry.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ipc from 'ipc';
 import { Provider } from 'react-redux';
 import store from './store';
+import injectTapEventPlugin from "react-tap-event-plugin";
+
+injectTapEventPlugin();
 
 ipc.on('app', (app) => {
   const path = `./components/${app}.jsx`;
