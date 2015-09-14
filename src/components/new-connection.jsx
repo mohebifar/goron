77479ecb0b0e1ca95df5232a11feb47581b9cb4c
mohamad <netfars@gmail.com>
2015-09-14 12:52:29 +0430

    style: Complete the UI for new-connection window

--- src/components/new-connection.jsx
+++ src/components/new-connection.jsx
@@ -1,15 +1,14 @@
 import React, {Component, PropTypes} from 'react';
 import Radium from 'radium';
-import {TextField, Tabs, Tab, RaisedButton, IconButton} from 'material-ui';
-import {connectReduxForm} from 'redux-form';
+import { TextField, Tabs, Tab, RaisedButton, Checkbox } from 'material-ui';
+import { connectReduxForm } from 'redux-form';
 import mui from 'material-ui';
 
 @Radium
 @connectReduxForm({
   form: 'contact',
-  fields: ['name', 'host', 'port']
-})
-class Connections extends Component {
+  fields: ['name', 'host', 'port', 'adminDatabase', 'username', 'password']
+}) class Connections extends Component {
   static childContextTypes = {
     muiTheme: PropTypes.object
   };
@@ -33,44 +32,75 @@ class Connections extends Component {
   }
 
   render() {
-    const { fields: {name, host, port}, handleSubmit } = this.props;
+    const { fields: {name, host, port, adminDatabase, username, password}, handleSubmit } = this.props;
 
-    return (<Tabs>
-      <Tab label="Connection">
-        <div style={styles.wrapper}>
-          <div style={styles.formGroup}>
-            <TextField
-              style={styles.input}
-              hintText="New Connection"
-              {...name}
-              floatingLabelText="The connection name"/>
+    return (<div>
+      <Tabs>
+        <Tab label="Connection">
+          <div style={styles.wrapper}>
+            <div style={styles.formGroup}>
+              <TextField
+                style={styles.input}
+                hintText="New Connection"
+                {...name}
+                floatingLabelText="The connection name"/>
+            </div>
+
+            <div style={styles.formGroup}>
+              <TextField
+                style={styles.input}
+                hintText="eg. 127.0.0.1"
+                {...host}
+                floatingLabelText="Host"/>
+            </div>
+
+            <div style={styles.formGroup}>
+              <TextField
+                style={styles.input}
+                hintText="eg. 27017"
+                {...port}
+                floatingLabelText="Port"/>
+            </div>
           </div>
+        </Tab>
 
-          <div style={styles.formGroup}>
-            <TextField
-              style={styles.input}
-              hintText="eg. 127.0.0.1"
-              {...host}
-              floatingLabelText="Host"/>
+        <Tab label="Authentication">
+          <div style={styles.wrapper}>
+            <div style={styles.formGroup}>
+              <Checkbox
+                label="Perform Authentication"/>
+            </div>
+
+            <div style={styles.formGroup}>
+              <TextField
+                style={styles.input}
+                hintText="The admin database is unique in MongoDB."
+                {...adminDatabase}
+                floatingLabelText="Database"/>
+            </div>
+
+            <div style={styles.formGroup}>
+              <TextField
+                style={styles.input}
+                {...username}
+                floatingLabelText="Username"/>
+            </div>
+
+            <div style={styles.formGroup}>
+              <TextField
+                style={styles.input}
+                {...password}
+                floatingLabelText="Password"/>
+            </div>
           </div>
+        </Tab>
+      </Tabs>
 
-          <div style={styles.formGroup}>
-            <TextField
-              style={styles.input}
-              hintText="eg. 27017"
-              {...port}
-              floatingLabelText="Port"/>
-          </div>
-          <RaisedButton label="Create" primary={true} onClick={handleSubmit(this.saveConnection)}/>
-        </div>
-      </Tab>
-
-      <Tab label="Authentication">
-        <div style={styles.wrapper}>
-          <RaisedButton label="Create" primary={true} onClick={handleSubmit(this.saveConnection)}/>
-        </div>
-      </Tab>
-    </Tabs>);
+      <div style={styles.buttonsContainer}>
+        <RaisedButton style={styles.button} label="Test" secondary={true}/>
+        <RaisedButton style={styles.button} label="Create" primary={true} onClick={handleSubmit(this.saveConnection)}/>
+      </div>
+    </div>);
   }
 }
 
@@ -78,13 +108,24 @@ const themeManager = new mui.Styles.ThemeManager();
 
 const styles = {
   wrapper: {
-    padding: '0 15px'
+    padding: 15
   },
   formGroup: {
     marginBottom: 10
   },
   input: {
     width: '100%'
+  },
+  buttonsContainer: {
+    background: '#EEE',
+    padding: 15,
+    position: 'fixed',
+    bottom: 0,
+    left: 0,
+    right: 0
+  },
+  button: {
+    marginRight: 15
   }
 };
 
