cb76dfaf296190081bfa41c526b232ef61f38640
mohamad <netfars@gmail.com>
2015-09-18 02:10:38 +0430

    feat: Current connection reducer

--- src/reducers/current-connection.js
+++ src/reducers/current-connection.js
@@ -1,3 +1,21 @@
-/**
- * Created by mohamad on 9/17/15.
- */
+import { SELECT_CONNECTION } from './../actions/types';
+import store from './../store.js';
+import ipc from 'ipc';
+
+export default function (state, action) {
+  if (typeof state === 'undefined') {
+    state = {};
+  }
+
+  if (action.type === SELECT_CONNECTION) {
+    ipc.once('window-opened', function () {
+      window.close();
+    });
+
+    ipc.send('open-window', 'main');
+
+    return action.connection;
+  }
+
+  return state;
+}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { reducer as form } from 'redux-form';
 import connectionsList from './connections-list.js';
+import currentConnection from './current-connection.js';
 
 export default combineReducers({
   form,
-  connectionsList
+  connectionsList,
+  currentConnection
 });
